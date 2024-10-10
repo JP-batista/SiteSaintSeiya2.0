9e338392040a71e264073e43983479e2abe63f27
JP <jpedro204jp@rede.ulbra.br>
2024-10-10 00:04:16 -0300

    S

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // src/app/page.tsx
 
-import Link from 'next/link';
+import Link from 'next/link'; // Importando o componente Link para navegação
 
 export default function HomePage() {
   return (
@@ -27,27 +27,29 @@ export default function HomePage() {
       <section className="my-16">
         <h2 className="text-4xl font-extrabold text-yellow-400 text-center mb-10">Funcionalidades do Site</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300">
+          {/* Card 1 - Explorar Personagens */}
+          <Link href="/characters" className="bg-gray-800 p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300">
             <h3 className="text-2xl font-bold text-yellow-400 mb-4">Explorar Personagens</h3>
             <p className="text-gray-300 text-lg">
               Conheça os Cavaleiros de Bronze, de Ouro e os inimigos poderosos.
             </p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300">
+          </Link>
+
+          {/* Card 2 - Histórias Épicas */}
+          <Link href="/stories" className="bg-gray-800 p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300">
             <h3 className="text-2xl font-bold text-yellow-400 mb-4">Histórias Épicas</h3>
             <p className="text-gray-300 text-lg">
-              Descubra as batalhas épicas e os desafios enfrentados pelos Cavaleiros de Atena.
+              Descubra as batalhas mais épicas da série e o impacto dos Cavaleiros do Zodíaco.
             </p>
-          </div>
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300">
+          </Link>
+
+          {/* Card 3 - Linha do Tempo Interativa */}
+          <Link href="#timeline" className="bg-gray-800 p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300">
             <h3 className="text-2xl font-bold text-yellow-400 mb-4">Linha do Tempo Interativa</h3>
             <p className="text-gray-300 text-lg">
               Navegue pelas sagas e veja como elas se conectam na história dos Cavaleiros do Zodíaco.
             </p>
-            <Link href="#timeline" className="mt-4 block text-yellow-400 hover:text-yellow-500 transition-colors duration-300">
-              Acessar Linha do Tempo
-            </Link>
-          </div>
+          </Link>
         </div>
       </section>
 
@@ -56,6 +58,7 @@ export default function HomePage() {
         <h2 className="text-4xl font-extrabold text-yellow-400 text-center mb-10">Linha do Tempo das Sagas</h2>
         <div className="relative">
           <div className="border-l-4 border-yellow-400 absolute h-full left-1/2 transform -translate-x-1/2"></div>
+
           {/* Evento 1: Saga do Santuário */}
           <div className="mb-8 w-full">
             <div className="flex justify-between items-center">
@@ -70,7 +73,7 @@ export default function HomePage() {
                 <div className="bg-gray-900 bg-opacity-50 p-6 rounded-lg shadow-lg hover:bg-opacity-60 transition-all duration-300">
                   <h3 className="text-3xl font-bold text-yellow-400 mb-4">Saga do Santuário</h3>
                   <p className="text-gray-300 mb-4">
-                    A primeira grande batalha dos Cavaleiros de Bronze, onde enfrentam os Cavaleiros de Ouro nas 12 Casas do Zodíaco para salvar Atena e derrotar o Grande Mestre, que controla o Santuário.
+                    A primeira grande batalha dos Cavaleiros de Bronze, onde enfrentam os Cavaleiros de Ouro nas 12 Casas do Zodíaco para salvar Atena.
                   </p>
                   <Link href="/stories/santuary-saga" className="mt-4 block bg-yellow-500 text-gray-900 px-4 py-2 rounded-lg font-semibold text-center hover:bg-yellow-600 transition-colors duration-300">
                     Ver Detalhes
@@ -93,7 +96,7 @@ export default function HomePage() {
               <div className="bg-gray-900 bg-opacity-50 p-6 rounded-lg shadow-lg hover:bg-opacity-60 transition-all duration-300">
                 <h3 className="text-3xl font-bold text-yellow-400 mb-4">Saga de Asgard</h3>
                 <p className="text-gray-300 mb-4">
-                  Os Cavaleiros de Atena viajam ao Reino de Asgard para enfrentar os Guerreiros Deuses e salvar Hilda de Polaris, corrompida pelo Anel de Nibelungo.
+                  Os Cavaleiros de Atena viajam ao Reino de Asgard para enfrentar os Guerreiros Deuses e salvar Hilda de Polaris.
                 </p>
                 <Link href="/stories/asgard-saga" className="mt-4 block bg-yellow-500 text-gray-900 px-4 py-2 rounded-lg font-semibold text-center hover:bg-yellow-600 transition-colors duration-300">
                   Ver Detalhes
@@ -115,7 +118,7 @@ export default function HomePage() {
               <div className="bg-gray-900 bg-opacity-50 p-6 rounded-lg shadow-lg hover:bg-opacity-60 transition-all duration-300">
                 <h3 className="text-3xl font-bold text-yellow-400 mb-4">Saga de Poseidon</h3>
                 <p className="text-gray-300 mb-4">
-                  Os Cavaleiros enfrentam o deus Poseidon e seus Generais Marinas para salvar o mundo da inundação global planejada por ele.
+                  Os Cavaleiros enfrentam Poseidon e seus Generais Marinas para salvar o mundo da inundação planejada por ele.
                 </p>
                 <Link href="/stories/poseidon-saga" className="mt-4 block bg-yellow-500 text-gray-900 px-4 py-2 rounded-lg font-semibold text-center hover:bg-yellow-600 transition-colors duration-300">
                   Ver Detalhes
@@ -137,7 +140,7 @@ export default function HomePage() {
               <div className="bg-gray-900 bg-opacity-50 p-6 rounded-lg shadow-lg hover:bg-opacity-60 transition-all duration-300">
                 <h3 className="text-3xl font-bold text-yellow-400 mb-4">Saga de Hades</h3>
                 <p className="text-gray-300 mb-4">
-                  A saga final onde os Cavaleiros de Atena enfrentam Hades e seus Espectros para salvar o mundo do Grande Eclipse.
+                  Os Cavaleiros enfrentam Hades e seus Espectros para salvar o mundo do Grande Eclipse.
                 </p>
                 <Link href="/stories/hades-saga" className="mt-4 block bg-yellow-500 text-gray-900 px-4 py-2 rounded-lg font-semibold text-center hover:bg-yellow-600 transition-colors duration-300">
                   Ver Detalhes
@@ -147,55 +150,48 @@ export default function HomePage() {
           </div>
         </div>
       </section>
+
       {/* Destaque de Personagens */}
       <section className="my-16">
         <h2 className="text-4xl font-extrabold text-yellow-400 text-center mb-10">Personagens em Destaque</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          {/* Card 1 - Seiya */}
-          <Link href="/characters/bronze/seiya">
-            <div className="bg-gray-800 rounded-lg shadow-lg p-6 hover:scale-105 transition-transform duration-300 cursor-pointer">
-              <img
-                src="https://i.pinimg.com/originals/f4/78/66/f47866e3aa44f46a7857548f22526aa7.png"
-                alt="Seiya de Pégaso"
-                className="w-60 h-88 object-cover rounded-lg mb-4"
-              />
-              <h3 className="text-2xl text-yellow-300 font-bold mb-2">Seiya de Pégaso</h3>
-              <p className="text-gray-300 text-lg">O Cavaleiro de Bronze que luta pela justiça e por Atena.</p>
-            </div>
+          <Link href="/characters/bronze/seiya" className="bg-gray-800 rounded-lg shadow-lg p-6 hover:scale-105 transition-transform duration-300 cursor-pointer">
+            <img
+              src="https://i.pinimg.com/originals/f4/78/66/f47866e3aa44f46a7857548f22526aa7.png"
+              alt="Seiya de Pégaso"
+              className="w-60 h-88 object-cover rounded-lg mb-4"
+            />
+            <h3 className="text-2xl text-yellow-300 font-bold mb-2">Seiya de Pégaso</h3>
+            <p className="text-gray-300 text-lg">O Cavaleiro de Bronze que luta pela justiça e por Atena.</p>
           </Link>
 
-          {/* Card 2 - Shiryu */}
-          <Link href="/characters/bronze/shiryu">
-            <div className="bg-gray-800 rounded-lg shadow-lg p-6 hover:scale-105 transition-transform duration-300 cursor-pointer">
-              <img
-                src="https://i.pinimg.com/originals/be/e9/8b/bee98b455b2807ce86f50051d52767c3.png"
-                alt="Shiryu de Dragão"
-                className="w-60 h-88 object-cover rounded-lg mb-4"
-              />
-              <h3 className="text-2xl text-yellow-300 font-bold mb-2">Shiryu de Dragão</h3>
-              <p className="text-gray-300 text-lg">O nobre e corajoso Cavaleiro de Dragão, protetor do Santuário.</p>
-            </div>
+          <Link href="/characters/bronze/shiryu" className="bg-gray-800 rounded-lg shadow-lg p-6 hover:scale-105 transition-transform duration-300 cursor-pointer">
+            <img
+              src="https://i.pinimg.com/originals/be/e9/8b/bee98b455b2807ce86f50051d52767c3.png"
+              alt="Shiryu de Dragão"
+              className="w-60 h-88 object-cover rounded-lg mb-4"
+            />
+            <h3 className="text-2xl text-yellow-300 font-bold mb-2">Shiryu de Dragão</h3>
+            <p className="text-gray-300 text-lg">O nobre e corajoso Cavaleiro de Dragão, protetor do Santuário.</p>
           </Link>
 
-          {/* Card 3 - Hyoga */}
-          <Link href="/characters/bronze/hyoga">
-            <div className="bg-gray-800 rounded-lg shadow-lg p-6 hover:scale-105 transition-transform duration-300 cursor-pointer">
-              <img
-                src="https://i.pinimg.com/originals/6c/52/e9/6c52e90d72b7d9aea6ca9cf6a95fd8f9.png"
-                alt="Hyoga de Cisne"
-                className="w-60 h-88 object-cover rounded-lg mb-4"
-              />
-              <h3 className="text-2xl text-yellow-300 font-bold mb-2">Hyoga de Cisne</h3>
-              <p className="text-gray-300 text-lg">O Cavaleiro de Cisne, cuja frieza esconde um grande coração.</p>
-            </div>
+          <Link href="/characters/bronze/hyoga" className="bg-gray-800 rounded-lg shadow-lg p-6 hover:scale-105 transition-transform duration-300 cursor-pointer">
+            <img
+              src="https://i.pinimg.com/originals/6c/52/e9/6c52e90d72b7d9aea6ca9cf6a95fd8f9.png"
+              alt="Hyoga de Cisne"
+              className="w-60 h-88 object-cover rounded-lg mb-4"
+            />
+            <h3 className="text-2xl text-yellow-300 font-bold mb-2">Hyoga de Cisne</h3>
+            <p className="text-gray-300 text-lg">O Cavaleiro de Cisne, cuja frieza esconde um grande coração.</p>
           </Link>
         </div>
       </section>
+
       {/* Footer */}
       <footer className="text-center my-12">
-        <a href="/characters" className="bg-yellow-500 text-gray-900 px-6 py-3 rounded-lg font-semibold text-lg hover:bg-yellow-600 transition-colors duration-300">
+        <Link href="/characters" className="bg-yellow-500 text-gray-900 px-6 py-3 rounded-lg font-semibold text-lg hover:bg-yellow-600 transition-colors duration-300">
           Explore Todos os Personagens
-        </a>
+        </Link>
       </footer>
     </div>
   );
