2981f76899045d9c7447d955541b07cf95823e95
JP <jpedro204jp@rede.ulbra.br>
2024-11-09 19:56:11 -0300

    Skins

--- src/app/data/skins.ts
+++ src/app/data/skins.ts
@@ -67,19 +67,223 @@ export interface Cavaleiro {
         banner: "/skins/banner-ikkidivino.jpg",
     },
     {
-      name: "Mu de Áries",
-      banner: "/skins/banner-mu1.jpg",
+        name: "Jabu de Unicórnio",
+        banner: "/skins/banner-jabu.jpg",
     },
     {
-      name: "Aldebaran de Touro",
-      banner: "/skins/banner-aldebaran1.jpg",
+        name: "Geki de Urso",
+        banner: "/skins/banner-geki.jpg",
     },
     {
-      name: "Saga de Gêmeos",
-      banner: "/skins/banner-saga1.jpg",
+        name: "Ichi de Hidra",
+        banner: "/skins/banner-ichi.jpg",
+    },
+    {
+        name: "Nachi de Lobo",
+        banner: "/skins/banner-nachi.jpg",
+    },
+    {
+        name: "Ban de Leão Menor",
+        banner: "/skins/banner-ban.jpg",
+    },
+    {
+        name: "Tenma de Pégaso",
+        banner: "/skins/banner-tenma1.jpg",
+    },
+    {
+        name: "Mu de Áries 1",
+        banner: "/skins/banner-mu1.jpg",
+    },
+    {
+        name: "Mu de Áries 2",
+        banner: "/skins/banner-mu2.jpg",
+    },
+    {
+        name: "Aldebaran de Touro",
+        banner: "/skins/banner-aldebaran1.jpg",
+    },
+    {
+        name: "Armadura de Gêmeos",
+        banner: "/skins/banner-saga1.jpg",
+    },
+    {
+        name: "Saga de Gêmeos",
+        banner: "/skins/banner-saga2.jpg",
+    },
+    {
+        name: "Máscara da Morte de Câncer 1",
+        banner: "/skins/banner-cancer1.jpg",
+    },
+    {
+        name: "Máscara da Morte de Câncer 2",
+        banner: "/skins/banner-cancer2.jpg",
+    },
+    {
+        name: "Aiolia de Leão 1",
+        banner: "/skins/banner-leao1.jpg",
+    },
+    {
+        name: "Aiolia de Leão 2",
+        banner: "/skins/banner-leao2.jpg",
+    },
+    {
+        name: "Shaka de Virgem",
+        banner: "/skins/banner-virgem1.jpg",
+    },
+    {
+        name: "Dohko de Libra",
+        banner: "/skins/banner-libra1.jpg",
+    },
+    {
+        name: "Mestre Anciâo",
+        banner: "/skins/banner-libra2.jpg",
+    },
+    {
+        name: "Milo de Escorpião",
+        banner: "/skins/banner-milo1.jpg",
+    },
+    {
+        name: "Aiolos de Sagitário",
+        banner: "/skins/banner-aiolos1.jpg",
+    },
+    {
+        name: "Shura de Capricórnio",
+        banner: "/skins/banner-shura1.jpg",
+    },
+    {
+        name: "Camus de Aquário",
+        banner: "/skins/banner-camus1.jpg",
+    },
+    {
+        name: "Afrodite de Peixes",
+        banner: "/skins/banner-afrodite1.jpg",
+    },
+    {
+        name: "Mestre Ares",
+        banner: "/skins/banner-mestreares.jpg",
+    },
+    {
+        name: "Marin de Águia",
+        banner: "/skins/banner-marin1.jpg",
+    },
+    {
+        name: "Shina de Cobra",
+        banner: "/skins/banner-shina1.jpg",
+    },
+    {
+        name: "Jamian de Corvo",
+        banner: "/skins/banner-corvo1.jpg",
+    },
+    {
+        name: "Aracne de Tarârantula",
+        banner: "/skins/banner-aracne.jpg",
+    },
+    {
+        name: "Dante de Cérberu",
+        banner: "/skins/banner-dante.jpg",
+    },
+    {
+        name: "Capella de Auriga",
+        banner: "/skins/banner-capela.jpg",
+    },
+    {
+        name: "Mestre Cristal",
+        banner: "/skins/banner-cristal.jpg",
+    },
+    {
+        name: "Cavaleiro do Fogo",
+        banner: "/skins/banner-fogo.jpg",
+    },
+    {
+        name: "Thor de Phecda",
+        banner: "/skins/banner-thor.jpg",
+    },
+    {
+        name: "Fenrir de Alioth",
+        banner: "/skins/banner-fenrir.jpg",
+    },
+    {
+        name: "Hagen de Merak",
+        banner: "/skins/banner-hagen.jpg",
+    },
+    {
+        name: "Mime de Benetnasch",
+        banner: "/skins/banner-mime.jpg",
+    },
+    {
+        name: "Alberich de Megrez",
+        banner: "/skins/banner-alberich.jpg",
+    },
+    {
+        name: "Shido de Mizar",
+        banner: "/skins/banner-shido.jpg",
+    },
+    {
+        name: "Bado de Alkor",
+        banner: "/skins/banner-bado.jpg",
+    },
+    {
+        name: "Siegfrid de Dubhe",
+        banner: "/skins/banner-siegfried.jpg",
+    },
+    {
+        name: "Hilda de Polaris",
+        banner: "/skins/banner-hilda.jpg",
+    },
+    {
+        name: "Thetis de Sereia",
+        banner: "/skins/banner-thetis.jpg",
+    },
+    {
+        name: "Io de Scylla",
+        banner: "/skins/banner-io.jpg",
+    },
+    {
+        name: "Kanon de Dragão Marinho",
+        banner: "/skins/banner-kanon1.jpg",
+    },
+    {
+        name: "Krishna de Chrysaor",
+        banner: "/skins/banner-krishna.jpg",
+    },
+    {
+        name: "Kasa de Lymnades",
+        banner: "/skins/banner-kasa.jpg",
+    },
+    {
+        name: "Sorento de Sirene",
+        banner: "/skins/banner-sorento.jpg",
+    },
+    {
+        name: "Poseidon",
+        banner: "/skins/banner-poseidon.jpg",
+    },
+    {
+        name: "Abel",
+        banner: "/skins/banner-abel.jpg",
+    },
+    {
+        name: "Marte",
+        banner: "/skins/banner-marte.jpg",
+    },
+    {
+        name: "Lune de Balron",
+        banner: "/skins/banner-lune.jpg",
+    },
+    {
+        name: "Atlas de Carena",
+        banner: "/skins/banner-atlas.jpg",
+    },
+    {
+        name: "Berengue de Coma Berenices",
+        banner: "/skins/banner-berengue.jpg",
+    },
+    {
+        name: "Jaô de Lince",
+        banner: "/skins/banner-jao.jpg",
     },
     // Adicione outros cavaleiros aqui
-  ];
-  
-  export default cavaleiros;
+];
+
+export default cavaleiros;
   
